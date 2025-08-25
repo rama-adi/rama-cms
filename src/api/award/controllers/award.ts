417c6949e36c8be5966a6b2c16f7f0f5d7f3df6c
ramaadi <17980909+rama-adi@users.noreply.github.com>
2025-08-25 13:36:26 +0800

    add award

--- src/api/award/controllers/award.ts
+++ src/api/award/controllers/award.ts
@@ -2,6 +2,48 @@
  * award controller
  */
 
+
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::award.award');
+export default factories.createCoreController('api::award.award', ({ strapi }) => ({
+    async find(ctx) {
+        ctx.query = {
+            ...ctx.query,
+            populate: {
+                mainImage: true
+            },
+        };
+
+        const { data, meta } = await super.find(ctx);
+
+        const formattedData = data.map(d => ({
+            id: d.documentId,
+            title: d.title,
+            mainImage: d.mainImage
+        }))
+
+
+        return { data: formattedData, meta };
+    },
+
+    async findOne(ctx) {
+        ctx.query = {
+            ...ctx.query,
+            populate: {
+                mainImage: true
+            },
+        };
+
+        const { data } = await super.findOne(ctx);
+
+
+
+        return {
+            id: data.documentId,
+            title: data.title,
+            mainImage: data.mainImage,
+            description: data.description
+        };
+    }
+}));
+
